043370bf38477e217b1b4df0b20d762e36e9fc98
emreerdall <emreerdal465@gmail.com>
2021-01-23 20:52:48 +0300

    Add project files.

---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
